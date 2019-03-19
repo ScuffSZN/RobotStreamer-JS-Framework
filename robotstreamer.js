07714c04bd307eabe8194527272900f625d332d2
ScuffedDev <45866296+ScuffedDev@users.noreply.github.com>
2019-03-18 22:44:17 -0400

    new functions + endpoints changes and features
    Add new functions + endpoints and changed one.
    - getControls([robot_id])
    + getRobotControls([robot_id])
    + getRobotIndex()
    + getTTSPrice([robot_id])
    + login({"password": "Account Password", "user_name": "Account Username"})
    + getUserSettings({token": "Token Passed by Login", "user_name": "Account Username"})

--- robotstreamer.js
+++ robotstreamer.js
@@ -23,13 +23,13 @@ var getVideo = (id) => {
     getServers = (id) => {
         return Promise.all([getVideo(id), getAudio(id)])
     },
-    getControls = (id) => {
+    getRobotControls = (id) => {
         return new Promise(resolve => {
             var j = {};
             fetch(`http://api.robotstreamer.com:8080//v1/get_endpoint/rscontrol_robot/${id}`)
                 .then((res) => res.json())
                 .then(function (json) {
-                    j = json.host+":"+json.port;
+                    j = json.host + ":" + json.port;
                     resolve(j);
                 })
         });
@@ -51,8 +51,12 @@ var getVideo = (id) => {
                             else
                                 j.online = false
 
-                            j.camera_id = json[0].camera_id
-                            j.robot_id = json[0].robot_id
+                            j.camera_id = json[0].camera_id;
+                            j.robot_id = json[0].robot_id;
+                            j.viewers = json[0].viewers;
+                            j.name = json[0].robot_name;
+                            j.owner_id = json[0].owner_id
+                            j.controls = json[0].panels;
                             resolve(j);
                         } else {
                             j.error = 1;
@@ -67,5 +71,121 @@ var getVideo = (id) => {
                 j.error = 2;
                 resolve(j);
             }
+        });
+    },
+    getRobotIndex = () => {
+        return new Promise(resolve => {
+            var j = {};
+            fetch(`http://api.robotstreamer.com:8080/v1/get_all_robots`)
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function (json) {
+                    if (json.length != 0) {
+                        j.error = 0;
+                        j.robots = json;
+                        resolve(j);
+                    } else {
+                        j.error = 1;
+                        resolve(j);
+                    }
+                })
+                .catch(function (error) {
+                    j.error = 4;
+                    resolve(j);
+                });
+        });
+    },
+    getTTSPrice = (id) => {
+        return new Promise(resolve => {
+            var j = {};
+            fetch(`http://api.robotstreamer.com:8080/v1/get_tts_price/${id}`)
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function (json) {
+                    if (json.length != 0) {
+                        j.error = 0;
+                        j.price = json.tts_price;
+                        resolve(j);
+                    } else {
+                        j.error = 1;
+                        resolve(j);
+                    }
+                })
+                .catch(function (error) {
+                    j.error = 4;
+                    resolve(j);
+                });
+        });
+    },
+    login = (uspw) => {
+        return new Promise(resolve => {
+            var j = {},
+                //data = {},
+                options = {
+                    method: "POST", // *GET, POST, PUT, DELETE, etc.
+                    mode: "cors", // no-cors, cors, *same-origin
+                    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: "same-origin", // include, *same-origin, omit
+                    headers: {
+                        "Content-Type": "application/json",
+                        // "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    redirect: "follow", // manual, *follow, error
+                    referrer: "no-referrer", // no-referrer, *client
+                    body: JSON.stringify(uspw), // body data type must match "Content-Type" header
+                }
+            fetch(`http://api.robotstreamer.com:8080/v1/login`,options)
+                .then(res => res.json())
+                .then(function (json) {
+                    if (json.length != 0) {
+                        j.error = 0;
+                        j.data = json;
+                        resolve(j);
+                    } else {
+                        j.error = 1;
+                        resolve(j);
+                    }
+                })
+                .catch(function (error) {
+                    j.error = 4;
+                    resolve(j);
+                });
+        });
+    },
+    getUserSettings = (uspw) => {
+        return new Promise(resolve => {
+            var j = {},
+                //data = {},
+                options = {
+                    method: "POST", // *GET, POST, PUT, DELETE, etc.
+                    mode: "cors", // no-cors, cors, *same-origin
+                    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: "same-origin", // include, *same-origin, omit
+                    headers: {
+                        "Content-Type": "application/json",
+                        // "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    redirect: "follow", // manual, *follow, error
+                    referrer: "no-referrer", // no-referrer, *client
+                    body: JSON.stringify(uspw), // body data type must match "Content-Type" header
+                }
+            fetch(`http://api.robotstreamer.com:8080/v1/get_user_settings`,options)
+                .then(res => res.json())
+                .then(function (json) {
+                    if (json.length != 0 && json.message != "Internal Server Error") {
+                        j.error = 0;
+                        j.data = json;
+                        resolve(j);
+                    } else {
+                        j.error = 4;
+                        resolve(j);
+                    }
+                })
+                .catch(function (error) {
+                    j.error = 4;
+                    resolve(j);
+                });
         });
     }
